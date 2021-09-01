fe3b40c7aecc61998d56cf0c041dc2886dd850bc
Lucas BOKSER <lucas.bokser@Digital-Grenoble.local>
2021-09-01 10:11:28 +0200

    changements

--- projet-spiruline-ilot-3/src/App.vue
+++ projet-spiruline-ilot-3/src/App.vue
@@ -4,92 +4,70 @@
 
     <div id="nav">
 
-    <div>
+      <div>
 
-      <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="/">Home</b-navbar-brand>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+          <b-navbar-brand href="/">Home</b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-
-
-              <router-link to="/customersList" tag="li">Go to Customer</router-link>
-
-              <router-link to="/customersList"
-                >Go to Customer</router-link
-              ></b-nav-item
-            >
-            <b-nav-item>
-              <router-link to="/ordersList"
-                >Go to Order</router-link
-              ></b-nav-item
-            >
-            <b-nav-item>
-              <router-link to="/recellersList"
-                >Go to Receller</router-link
-              ></b-nav-item
-            >
-            <b-nav-item>
-              <router-link to="/suppliersList"
-                >Go to Supplier</router-link
-              ></b-nav-item
-            >
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
 
 
-            <b-nav-item>
-              <router-link to="/customersList" tag="cx-button">Go to Customer</router-link>
+              <b-nav-item>
+                <router-link to="/customersList" tag="cx-button">Go to Customer</router-link>
 
-            </b-nav-item>
-            <b-nav-item>
-              <router-link to="/ordersList" tag="li">Go to Order</router-link>
-            </b-nav-item>
-            <b-nav-item>
-              <router-link to="/recellersList" tag="li">Go to Receller</router-link>
-            </b-nav-item>
-            <b-nav-item>
-              <router-link to="/suppliersList" tag="li">Go to Supplier</router-link>
-            </b-nav-item>
-
-
-          </b-navbar-nav>
-
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <b-form-input
-                size="sm"
-                class="mr-sm-2"
-                placeholder="Search"
-              ></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit"
-                >Search</b-button
-              >
-            </b-nav-form>
+              </b-nav-item>
+              <b-nav-item>
+                <router-link to="/ordersList" tag="li">Go to Order</router-link>
+              </b-nav-item>
+              <b-nav-item>
+                <router-link to="/recellersList" tag="li">Go to Receller</router-link>
+              </b-nav-item>
+              <b-nav-item>
+                <router-link to="/suppliersList" tag="li">Go to Supplier</router-link>
+              </b-nav-item>
 
 
+            </b-navbar-nav>
 
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template #button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-form>
+                <b-form-input
+                    size="sm"
+                    class="mr-sm-2"
+                    placeholder="Search"
+                ></b-form-input>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                >Search
+                </b-button
+                >
+              </b-nav-form>
+
+
+              <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template #button-content>
+                  <em>User</em>
+                </template>
+                <b-dropdown-item href="#">Profile</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
+      </div>
+
+      <div id="ontente">
+
+      </div>
+      <!--<HelloWorld msg="Ca Wax pour moi"/>-->
+
+
+      <router-view></router-view>
     </div>
-
-    <div id="ontente">
-
-    </div>
-    <!--<HelloWorld msg="Ca Wax pour moi"/>-->
-
-
-    <router-view></router-view>
   </div>
 </template>
 
@@ -99,23 +77,12 @@
 //import HelloWorld from './components/HelloWorld.vue'
 
 
-
 export default {
   name: "App",
   components: {
     //HelloWorld,
     //Home,
   },
-<<<<<<< HEAD
-=======
-
-
-  },
-};
-
-  }
-
->>>>>>> 311940756818e5177bf687722a47490a01adfe96
 }
 
 </script>
@@ -131,7 +98,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
 
 
 }
